3212abd2bffc95d6bf62921734750bb249914196
johngrantuk <johngrantuk@googlemail.com>
2020-11-26 15:55:25 +0000

    Removed dist folder.

--- dist/multicall.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseProvider } from '@ethersproject/providers';
-import { Pools, SubGraphPools } from './types';
-export declare function getAllPoolDataOnChain(
-    pools: SubGraphPools,
-    multiAddress: string,
-    provider: BaseProvider
-): Promise<Pools>;
--- dist/utils/bignumber.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const bignumber_js_1 = require('bignumber.js');
-exports.BigNumber = bignumber_js_1.BigNumber;
-bignumber_js_1.BigNumber.config({
-    EXPONENTIAL_AT: [-100, 100],
-    ROUNDING_MODE: 1,
-    DECIMAL_PLACES: 18,
-});
