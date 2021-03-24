8ddbb24b8c106dc5d12ddceabc73ad0859e5e276
johngrantuk <johngrantuk@googlemail.com>
2021-03-24 10:50:18 +0000

    Added limit test example.

--- test/testScripts/example-simpleSwapExactIn.ts
+++ test/testScripts/example-simpleSwapExactIn.ts
@@ -6,17 +6,31 @@ import { BigNumber } from 'bignumber.js';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
 import { Contract } from '@ethersproject/contracts';
+import { MaxUint256 } from '@ethersproject/constants';
+
+import vaultArtifact from '../../src/abi/vault.json';
+import erc20abi from '../../src/abi/ERC20.json';
 
 export type FundManagement = {
+    sender: string;
     recipient: string;
     fromInternalBalance: boolean;
     toInternalBalance: boolean;
 };
 
-// TODO - Remove if unused in mainnet deploy
-const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // DAI Address
-const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // USDC Address
+// Taken from V2_core repo
+const maxInt = new BigNumber(2)
+    .pow(new BigNumber(256).minus(new BigNumber(1)))
+    .minus(new BigNumber(1));
+
+// rc01 Kovan addresses
+const WETH = '0xe1329748c41A140536e41049C95c36A53bCACee6';
+const BAL = '0x1688C45BC51Faa1B783D274E03Da0A0B28A0A871';
+const MKR = '0xD9D9E09604c0C14B592e6E383582291b026EBced';
+const vaultAddr = '0xba1c01474A7598c2B49015FdaFc67DdF06ce15f7';
+
+// TODO - Update with ipns when ready.
+const poolsUrl = `https://storageapi.fleek.co/johngrantuk-team-bucket/poolsRc1-22-03.json`;
 
 async function simpleSwap() {
     // If running this example make sure you have a .env file saved in root DIR with INFURA=your_key
@@ -36,22 +50,12 @@ async function simpleSwap() {
     // This determines the max no of pools the SOR will use to swap.
     const maxNoPools = 4;
     const chainId = 42;
-    // TODO - Update with ipns when ready.
-    const poolsUrl = `https://storageapi.fleek.co/johngrantuk-team-bucket/poolsV2.json`;
+    const tokenIn = BAL;
+    const tokenOut = MKR;
+    const swapType = 'swapExactIn'; // Two different swap types are used: swapExactIn & swapExactOut
+    const amountIn = new BigNumber(0.1); // In normalized format, i.e. 1USDC = 1
 
     const sor = new SOR(provider, gasPrice, maxNoPools, chainId, poolsUrl);
-
-    const tokenIn = '0xfa06b7b5e149e575b457e595c606ec58b17e9e13';
-    const tokenOut = '0xb5399358fa9744c604f8fae7043a547f74206d4c';
-    const swapType = 'swapExactIn'; // Two different swap types are used: swapExactIn & swapExactOut
-    let amountIn = new BigNumber(0.1); // In normalized format, i.e. 1USDC = 1
-
-    // This can be used to check if all pools have been fetched
-    let isFinishedFetchingOnChain = sor.finishedFetchingOnChain;
-
-    // Can be used to check if pair/pools been fetched
-    let hasDataForPair = sor.hasDataForPair(tokenIn, tokenOut);
-    console.log(`isFinishedFetchingOnChain ${isFinishedFetchingOnChain}`);
 
     // This calculates the cost to make a swap which is used as an input to sor to allow it to make gas efficient recommendations.
     // Can be set once and will be used for further swap calculations.
@@ -59,8 +63,8 @@ async function simpleSwap() {
     await sor.setCostOutputToken(tokenOut);
 
     // This fetches all pools list from URL in constructor then onChain balances using Multicall
-    let fetch = await sor.fetchPools();
-    isFinishedFetchingOnChain = sor.finishedFetchingOnChain;
+    await sor.fetchPools();
+    const isFinishedFetchingOnChain = sor.finishedFetchingOnChain;
     console.log(`isFinishedFetchingOnChain ${isFinishedFetchingOnChain}`);
 
     const swapInfo: SwapInfo = await sor.getSwaps(
@@ -73,32 +77,71 @@ async function simpleSwap() {
     console.log(swapInfo.returnAmount.toString());
     console.log(swapInfo.swaps);
 
-    // TODO - Update with deploy
-    const vaultAddr = '0x99EceD8Ba43D090CA4283539A31431108FD34438';
-    const validatorAddr = '0x6648473ae4D7a7FdE330846D11ee95FDE2DE9447';
-    const vaultArtifact = require('../../src/abi/vault.json');
+    // The rest of the code executes a swap using wallet funds
+
+    /*
+    // Vault needs approval for swapping
+    console.log('Approving vault...');
+    let tokenInContract = new Contract(
+        tokenIn,
+        erc20abi,
+        provider
+    );
+
+    let txApprove = await tokenInContract.connect(wallet).approve(vaultAddr, MaxUint256);
+    console.log(txApprove);
+    */
 
     const vaultContract = new Contract(vaultAddr, vaultArtifact, provider);
     vaultContract.connect(wallet);
 
     const funds: FundManagement = {
+        sender: wallet.address,
         recipient: wallet.address,
         fromInternalBalance: false,
         toInternalBalance: false,
     };
 
-    const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+    // Limits:
+    // +ve means max to send
+    // -ve mean min to receive
+    // For a multihop the intermediate tokens should be ok at 0?
 
-    // TODO - Add approve?
+    const limits = [];
+
+    swapInfo.tokenAddresses.forEach((token, i) => {
+        if (token.toLowerCase() === tokenIn.toLowerCase()) {
+            console.log('Token In');
+            // TO DO
+            limits[i] = maxInt.toString();
+        } else if (token.toLowerCase() === tokenOut.toLowerCase()) {
+            console.log('Token Out');
+            // This should be amt + slippage in UI
+            limits[i] = swapInfo.returnAmount.times(-1).toString();
+        } else {
+            console.log('Token Intermmediate');
+            limits[i] = maxInt.toString();
+            // To do - this doesn't work?
+            // limits[i] = '0';
+        }
+    });
+
+    console.log(limits);
+    //
+    const deadline = MaxUint256;
 
     let tx = await vaultContract
         .connect(wallet)
         .batchSwapGivenIn(
-            ZERO_ADDRESS,
-            '0x',
             swapInfo.swaps,
             swapInfo.tokenAddresses,
-            funds
+            funds,
+            limits,
+            deadline,
+            {
+                gasLimit: '200000',
+                gasPrice: '20000000000',
+            }
         );
 
     console.log(`tx: ${tx.hash}`);
