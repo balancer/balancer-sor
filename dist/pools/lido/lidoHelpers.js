2efc7ea5155187b74615ad53819c684058ac6ab0
johngrantuk <johngrantuk@googlemail.com>
2021-08-10 10:07:16 +0100

    Build for package.

--- dist/pools/lido/lidoHelpers.js
+++ dist/pools/lido/lidoHelpers.js
@@ -909,9 +909,11 @@ function queryBatchSwap(swapType, swaps, assets, provider) {
                 funds
             );
             if (swapType === types_1.SwapTypes.SwapExactIn)
-                return deltas[assets.length - 1].mul(-1);
+                return bmath_1
+                    .bnum(deltas[assets.length - 1].toString())
+                    .times(-1);
             // negative amounts represent tokens (or ETH) sent by the Vault
-            else return deltas[0].mul(-1);
+            else return bmath_1.bnum(deltas[0].toString()).times(-1);
         } catch (err) {
             console.error(`SOR - queryBatchSwap: ${err.message}`);
             return bmath_1.bnum(0);
