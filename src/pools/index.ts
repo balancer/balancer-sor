415f7c097d61d46b50863452805cbf9bd9149bb5
Tom French <tom@tomfren.ch>
2021-09-03 12:47:58 +0100

    feat: add support for InvestmentPools

--- src/pools/index.ts
+++ src/pools/index.ts
@@ -15,19 +15,24 @@ export function parseNewPool(
     pool: SubgraphPoolBase,
     currentBlockTimestamp = 0
 ): WeightedPool | StablePool | ElementPool | undefined {
+    // We're not interested in any pools which don't allow swapping
+    // (Explicit check for false as many of the tests omit this flag)
+    if (pool.swapEnabled === false) return undefined;
+
     let newPool: WeightedPool | StablePool | ElementPool;
-    if (pool.poolType === 'Weighted') newPool = WeightedPool.fromPool(pool);
-    else if (pool.poolType === 'Stable') newPool = StablePool.fromPool(pool);
-    else if (pool.poolType === 'Element') {
+    if (
+        pool.poolType === 'Weighted' ||
+        pool.poolType === 'LiquidityBootstrapping' ||
+        pool.poolType === 'Investment'
+    ) {
+        newPool = WeightedPool.fromPool(pool);
+    } else if (pool.poolType === 'Stable') {
+        newPool = StablePool.fromPool(pool);
+    } else if (pool.poolType === 'Element') {
         newPool = ElementPool.fromPool(pool);
         newPool.setCurrentBlockTimestamp(currentBlockTimestamp);
     } else if (pool.poolType === 'MetaStable') {
         newPool = MetaStablePool.fromPool(pool);
-    } else if (pool.poolType === 'LiquidityBootstrapping') {
-        // If an LBP doesn't have its swaps paused we treat it like a regular Weighted pool.
-        // If it does we just ignore it.
-        if (pool.swapEnabled === true) newPool = WeightedPool.fromPool(pool);
-        else return undefined;
     } else {
         console.error(
             `Unknown pool type or type field missing: ${pool.poolType} ${pool.id}`
