f35a9f37c7ac59709dd8ee8a13c192a63daa4585
johngrantuk <johngrantuk@googlemail.com>
2021-09-10 11:33:46 +0100

    Linear token <> token swap test.

--- test/linear.spec.ts
+++ test/linear.spec.ts
@@ -15,19 +15,20 @@ import { calculatePathLimits } from '../src/routeProposal/pathLimits';
 import BigNumber from 'bignumber.js';
 import { formatSwaps } from '../src/formatSwaps';
 
-import subgraphPoolsLargeLinear from './testData/subgraphPoolsLargeLinear.json';
+import subgraphPoolsLargeLinear from './testData/linearPools/subgraphPoolsLargeLinear.json';
+import singleLinear from './testData/linearPools/singleLinear.json';
 import { MetaStablePool } from '../src/pools/metaStablePool/metaStablePool';
 import { bnum } from '../src/index';
 import { getBestPaths } from '../src/router';
 
-const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'; // WETH lower case
+const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'.toLowerCase();
-const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'.toLowerCase();
+const aDAI = '0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d';
 
 describe('Linear pools tests', () => {
     it('trivial check', async () => {
         const tokenIn = DAI;
-        const tokenOut = WETH; // USDC precision 6
+        const tokenOut = WETH;
         const swapType = SwapTypes.SwapExactIn;
         // Opción que va: DAI - USDC - 100
         const swapAmount = new BigNumber(0.1);
@@ -39,7 +40,67 @@ describe('Linear pools tests', () => {
         let hopTokens: string[];
         let linearPoolsInfo: [PoolDictionaryByMain, MetaStablePool];
         [pools, hopTokens, linearPoolsInfo] = filterPoolsOfInterest(
-            subgraphPoolsLargeLinear.pools,
+            cloneDeep(subgraphPoolsLargeLinear.pools),
+            tokenIn,
+            tokenOut,
+            maxPools,
+            chainId
+        );
+        let pathData: NewPath[];
+
+        [pools, pathData] = filterHopPools(tokenIn, tokenOut, hopTokens, pools);
+        const pathsUsingLinear = getPathsUsingLinearPools(
+            tokenIn,
+            tokenOut,
+            linearPoolsInfo,
+            pools
+        );
+        pathData = pathData.concat(pathsUsingLinear);
+        const [paths] = calculatePathLimits(pathData, swapType);
+
+        let swaps: any,
+            total: BigNumber,
+            totalConsideringFees: BigNumber,
+            marketSp: BigNumber;
+        [swaps, total, marketSp, totalConsideringFees] = getBestPaths(
+            cloneDeep(pools), // Need to keep original pools for cache
+            paths,
+            swapType,
+            swapAmount,
+            maxPools,
+            bnum(10) // costReturnToken
+        );
+        console.log(swaps);
+
+        const swapInfo = formatSwaps(
+            swaps,
+            swapType,
+            swapAmount,
+            tokenIn,
+            tokenOut,
+            total,
+            totalConsideringFees,
+            marketSp
+        );
+
+        console.log(swapInfo.swaps);
+        console.log(swapInfo.tokenAddresses);
+    });
+
+    it('token > token test', async () => {
+        const tokenIn = DAI;
+        const tokenOut = aDAI;
+        const swapType = SwapTypes.SwapExactIn;
+        const swapAmount = new BigNumber(10);
+        const chainId = 1;
+
+        // let paths: NewPath[], marketSp: BigNumber;
+        const maxPools = 10; //
+        let pools: PoolDictionary;
+        let hopTokens: string[];
+        let linearPoolsInfo: [PoolDictionaryByMain, MetaStablePool];
+        [pools, hopTokens, linearPoolsInfo] = filterPoolsOfInterest(
+            cloneDeep(singleLinear.pools),
             tokenIn,
             tokenOut,
             maxPools,
