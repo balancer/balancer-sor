7a4ee7e5176e98236601bbfbb8231dec4dceee4c
Tom French <tom@tomfren.ch>
2021-09-22 13:33:48 +0100

    fix: export stableHelpers again

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { SOR } from './wrapper';
 export { BPTForTokensZeroPriceImpact as weightedBPTForTokensZeroPriceImpact } from './frontendHelpers/weightedHelpers';
-// export { BPTForTokensZeroPriceImpact as stableBPTForTokensZeroPriceImpact } from './frontendHelpers/stableHelpers';
+export { BPTForTokensZeroPriceImpact as stableBPTForTokensZeroPriceImpact } from './frontendHelpers/stableHelpers';
 export { scale, bnum } from './utils/bignumber';
 export * from './types';
