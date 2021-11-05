87d27c2d2f98f2ef07d446835716762ec778aa81
sergioyuhjtman <sergioyuhjtman@gmail.com>
2021-11-05 18:10:41 -0300

    phantomStable: price factors and math tests; weightedMath tests

---
+++ test/phantomStableMath.spec.ts
@@ -0,0 +1,344 @@
+// TS_NODE_PROJECT='tsconfig.testing.json' npx mocha -r ts-node/register test/math.spec.ts
+import { assert } from 'chai';
+import { formatFixed } from '@ethersproject/bignumber';
+import { BigNumber as OldBigNumber, scale } from '../src/utils/bignumber';
+import { bnum } from '../src/utils/bignumber';
+import phantomStableStabal3WithPriceRates from './testData/phantomStablePools/phantomStableStabal3WithPriceRates.json';
+import {
+    PhantomStablePool,
+    PhantomStablePoolPairData,
+} from '../src/pools/phantomStablePool/phantomStablePool';
+import * as phantomStableMath from '../src/pools/phantomStablePool/phantomStableMath';
+
+describe('phantomStable pools tests', () => {
+    // For the moment we tolerate a moderate relative error until
+    // more accurate formulas are developed for phantomStable.
+    const error = 0.005;
+
+    context('phantomStable pools', () => {
+        const phantomStablePool = PhantomStablePool.fromPool(
+            phantomStableStabal3WithPriceRates.pools[0]
+        );
+        /*
+                "0x21ff756ca0cfcc5fff488ad67babadffee0c4149", // STABAL3
+                "0x3d1b554f1b1d1b6108b601ff22fea9c90fdfe50d", // LINEAR-AUSDC
+                "0x6a8c3239695613c0710dc971310b36f9b81e115e", // LINEAR-AUSDT
+                "0xcd32a460b6fecd053582e43b07ed6e2c04e15369" // LINEAR-ADAI
+*/
+        it('phantomStable token -> token', () => {
+            let poolPairData = phantomStablePool.parsePoolPairData(
+                '0x3d1b554f1b1d1b6108b601ff22fea9c90fdfe50d', // LINEAR-AUSDC
+                '0x6a8c3239695613c0710dc971310b36f9b81e115e' // LINEAR-AUSDT
+            );
+            let priceRateIn = bnum(
+                formatFixed(poolPairData.tokenInPriceRate, 18)
+            );
+            let priceRateOut = bnum(
+                formatFixed(poolPairData.tokenOutPriceRate, 18)
+            );
+            let { a1, a2 } = getSwapOutcomes(
+                phantomStablePool,
+                poolPairData,
+                4000
+            );
+            const b1 = phantomStableMath
+                ._exactTokenInForTokenOut(
+                    bnum(4000).times(priceRateIn),
+                    poolPairData
+                )
+                .div(priceRateOut);
+            const b2 = phantomStableMath
+                ._tokenInForExactTokenOut(
+                    bnum(4000).times(priceRateOut),
+                    poolPairData
+                )
+                .div(priceRateIn);
+            assert.approximately(
+                a1.div(b1).toNumber(),
+                1,
+                error,
+                'wrong result'
+            );
+            assert.approximately(
+                a2.div(b2).toNumber(),
+                1,
+                error,
+                'wrong result'
+            );
+            // spot price
+            checkPhantomStableSpotPrices(
+                phantomStablePool,
+                poolPairData,
+                4000,
+                error
+            );
+            checkPhantomStableDerivativeSpotPrices(
+                phantomStablePool,
+                poolPairData,
+                4000,
+                error
+            );
+        });
+
+        it('phantomStable BPT -> token', () => {
+            let poolPairData = phantomStablePool.parsePoolPairData(
+                '0x21ff756ca0cfcc5fff488ad67babadffee0c4149', // STABAL3
+                '0x3d1b554f1b1d1b6108b601ff22fea9c90fdfe50d' // LINEAR-AUSDC
+            );
+            let priceRateOut = bnum(
+                formatFixed(poolPairData.tokenOutPriceRate, 18)
+            );
+            // swap outcomes
+            const { a1, a2 } = getSwapOutcomes(
+                phantomStablePool,
+                poolPairData,
+                4000
+            );
+            const b1 = phantomStableMath
+                ._exactBPTInForTokenOut(bnum(4000), poolPairData)
+                .div(priceRateOut);
+            const b2 = phantomStableMath._BPTInForExactTokenOut(
+                bnum(4000).times(priceRateOut),
+                poolPairData
+            );
+            assert.approximately(
+                a1.div(b1).toNumber(),
+                1,
+                error,
+                'wrong result'
+            );
+            assert.approximately(
+                a2.div(b2).toNumber(),
+                1,
+                error,
+                'wrong result'
+            );
+            // spot prices
+            checkPhantomStableSpotPrices(
+                phantomStablePool,
+                poolPairData,
+                4000,
+                error
+            );
+        });
+        it('phantomStable token -> BPT', () => {
+            let poolPairData = phantomStablePool.parsePoolPairData(
+                '0x3d1b554f1b1d1b6108b601ff22fea9c90fdfe50d', // LINEAR-AUSDC
+                '0x21ff756ca0cfcc5fff488ad67babadffee0c4149' // STABAL3
+            );
+            const { a1, a2 } = getSwapOutcomes(
+                phantomStablePool,
+                poolPairData,
+                4000
+            );
+            let priceRateIn = bnum(
+                formatFixed(poolPairData.tokenInPriceRate, 18)
+            );
+            const b1 = phantomStableMath._exactTokenInForBPTOut(
+                bnum(4000).times(priceRateIn),
+                poolPairData
+            );
+            assert.approximately(
+                a1.div(b1).toNumber(),
+                1,
+                error,
+                'wrong result'
+            );
+            const b2 = phantomStableMath
+                ._tokenInForExactBPTOut(bnum(4000), poolPairData)
+                .div(priceRateIn);
+            assert.approximately(
+                a2.div(b2).toNumber(),
+                1,
+                error,
+                'wrong result'
+            );
+            // spot price:
+            checkPhantomStableSpotPrices(
+                phantomStablePool,
+                poolPairData,
+                4000,
+                error
+            );
+        });
+    });
+});
+
+function getSwapOutcomes(
+    phantomStablePool: PhantomStablePool,
+    poolPairData: PhantomStablePoolPairData,
+    amount: number
+): { a1: OldBigNumber; a2: OldBigNumber } {
+    const a1 = phantomStablePool._exactTokenInForTokenOut(
+        poolPairData,
+        bnum(amount),
+        true
+    );
+    const a2 = phantomStablePool._tokenInForExactTokenOut(
+        poolPairData,
+        bnum(amount),
+        true
+    );
+    return { a1, a2 };
+}
+
+function checkPhantomStableSpotPrices(
+    phantomStablePool: PhantomStablePool,
+    poolPairData: PhantomStablePoolPairData,
+    amount: number,
+    error: number
+) {
+    // exact token in
+    let a = incrementalQuotientExactTokenInForTokenOut(
+        phantomStablePool,
+        poolPairData,
+        amount,
+        0.01
+    );
+    let b = phantomStablePool._spotPriceAfterSwapExactTokenInForTokenOut(
+        poolPairData,
+        bnum(amount)
+    );
+    assert.approximately(
+        a.times(b).toNumber(),
+        1,
+        error,
+        'wrong result, spot price exact token in'
+    );
+    // exact token out
+    a = incrementalQuotientTokenInForExactTokenOut(
+        phantomStablePool,
+        poolPairData,
+        amount,
+        0.01
+    );
+    b = phantomStablePool._spotPriceAfterSwapTokenInForExactTokenOut(
+        poolPairData,
+        bnum(amount)
+    );
+    assert.approximately(
+        a.div(b).toNumber(),
+        1,
+        error,
+        'wrong result, spot price exact token out'
+    );
+}
+
+function incrementalQuotientExactTokenInForTokenOut(
+    phantomStablePool: PhantomStablePool,
+    poolPairData: PhantomStablePoolPairData,
+    amount: number,
+    delta: number
+): OldBigNumber {
+    const f1 = phantomStablePool._exactTokenInForTokenOut(
+        poolPairData,
+        bnum(amount + delta),
+        true
+    );
+    const f0 = phantomStablePool._exactTokenInForTokenOut(
+        poolPairData,
+        bnum(amount),
+        true
+    );
+    let incrementalQuotient = f1.minus(f0).div(delta);
+    return incrementalQuotient;
+}
+
+function incrementalQuotientTokenInForExactTokenOut(
+    phantomStablePool: PhantomStablePool,
+    poolPairData: PhantomStablePoolPairData,
+    amount: number,
+    delta: number
+): OldBigNumber {
+    const f1 = phantomStablePool._tokenInForExactTokenOut(
+        poolPairData,
+        bnum(amount + delta),
+        true
+    );
+    const f0 = phantomStablePool._tokenInForExactTokenOut(
+        poolPairData,
+        bnum(amount),
+        true
+    );
+    let incrementalQuotient = f1.minus(f0).div(delta);
+    return incrementalQuotient;
+}
+
+function checkPhantomStableDerivativeSpotPrices(
+    phantomStablePool: PhantomStablePool,
+    poolPairData: PhantomStablePoolPairData,
+    amount: number,
+    error: number
+) {
+    // exact token in
+    let a = incrementalQuotientSpotPriceAfterSwapExactTokenInForTokenOut(
+        phantomStablePool,
+        poolPairData,
+        amount,
+        0.01
+    );
+    let b =
+        phantomStablePool._derivativeSpotPriceAfterSwapExactTokenInForTokenOut(
+            poolPairData,
+            bnum(amount)
+        );
+    assert.approximately(
+        a.div(b).toNumber(),
+        1,
+        error,
+        'wrong result, derivative spot price exact token in'
+    );
+    // exact token out
+    a = incrementalQuotientSpotPriceAfterSwapTokenInForExactTokenOut(
+        phantomStablePool,
+        poolPairData,
+        amount,
+        0.01
+    );
+    b = phantomStablePool._derivativeSpotPriceAfterSwapTokenInForExactTokenOut(
+        poolPairData,
+        bnum(amount)
+    );
+    assert.approximately(
+        a.div(b).toNumber(),
+        1,
+        error,
+        'wrong result, derivative spot price exact token out'
+    );
+}
+
+function incrementalQuotientSpotPriceAfterSwapExactTokenInForTokenOut(
+    phantomStablePool: PhantomStablePool,
+    poolPairData: PhantomStablePoolPairData,
+    amount: number,
+    delta: number
+): OldBigNumber {
+    const f1 = phantomStablePool._spotPriceAfterSwapExactTokenInForTokenOut(
+        poolPairData,
+        bnum(amount + delta)
+    );
+    const f0 = phantomStablePool._spotPriceAfterSwapExactTokenInForTokenOut(
+        poolPairData,
+        bnum(amount)
+    );
+    let incrementalQuotient = f1.minus(f0).div(delta);
+    return incrementalQuotient;
+}
+
+function incrementalQuotientSpotPriceAfterSwapTokenInForExactTokenOut(
+    phantomStablePool: PhantomStablePool,
+    poolPairData: PhantomStablePoolPairData,
+    amount: number,
+    delta: number
+): OldBigNumber {
+    const f1 = phantomStablePool._spotPriceAfterSwapTokenInForExactTokenOut(
+        poolPairData,
+        bnum(amount + delta)
+    );
+    const f0 = phantomStablePool._spotPriceAfterSwapTokenInForExactTokenOut(
+        poolPairData,
+        bnum(amount)
+    );
+    let incrementalQuotient = f1.minus(f0).div(delta);
+    return incrementalQuotient;
+}
---
+++ test/weightedMath.spec.ts
@@ -0,0 +1,99 @@
+// TS_NODE_PROJECT='tsconfig.testing.json' npx mocha -r ts-node/register test/math.spec.ts
+import { assert } from 'chai';
+import { BigNumber as OldBigNumber, scale } from '../src/utils/bignumber';
+import { bnum } from '../src/utils/bignumber';
+import { BAL, DAI, USDC, WETH, USDT, STABAL3PHANTOM } from './lib/constants';
+import singleWeightedPool from './testData/weightedPools/singlePoolWithSwapEnabled.json';
+import {
+    WeightedPool,
+    WeightedPoolPairData,
+} from '../src/pools/weightedPool/weightedPool';
+
+describe('weightedMath tests', () => {
+    // TO DO: add items using checkOutcome function
+    context('spot prices', () => {
+        const weightedPool = WeightedPool.fromPool(singleWeightedPool.pools[0]);
+        const weightedPoolPairData = weightedPool.parsePoolPairData(
+            WETH.address,
+            BAL.address
+        );
+        it('weighted _spotPriceAfterSwapExactTokenInForTokenOut', () => {
+            checkDerivative(
+                weightedPool._exactTokenInForTokenOut,
+                weightedPool._spotPriceAfterSwapExactTokenInForTokenOut,
+                weightedPoolPairData,
+                1,
+                0.001,
+                0.00000001,
+                true
+            );
+        });
+        it('weighted _spotPriceAfterSwapTokenInForExactTokenOut', () => {
+            checkDerivative(
+                weightedPool._tokenInForExactTokenOut,
+                weightedPool._spotPriceAfterSwapTokenInForExactTokenOut,
+                weightedPoolPairData,
+                10,
+                0.01,
+                0.00000001,
+                false
+            );
+        });
+        it('weighted _derivativeSpotPriceAfterSwapExactTokenInForTokenOut', () => {
+            checkDerivative(
+                weightedPool._spotPriceAfterSwapExactTokenInForTokenOut,
+                weightedPool._derivativeSpotPriceAfterSwapExactTokenInForTokenOut,
+                weightedPoolPairData,
+                1,
+                0.001,
+                0.0000001,
+                false
+            );
+        });
+    });
+});
+
+function checkOutcome(
+    fn: (
+        poolPairData: any,
+        amount: OldBigNumber,
+        exact: boolean
+    ) => OldBigNumber,
+    poolPairData: any,
+    amount: number,
+    error: number
+) {
+    assert.approximately(
+        fn(poolPairData, bnum(amount), true).toNumber(),
+        fn(poolPairData, bnum(amount), false).toNumber(),
+        error,
+        'wrong result'
+    );
+}
+
+function checkDerivative(
+    fn: (
+        poolPairData: any,
+        amount: OldBigNumber,
+        exact: boolean
+    ) => OldBigNumber,
+    der: (poolPairData: any, amount: OldBigNumber) => OldBigNumber,
+    poolPairData: any,
+    amount: number,
+    delta: number,
+    error: number,
+    inverse: boolean = false
+) {
+    let x = bnum(amount);
+    let incrementalQuotient = fn(poolPairData, x.plus(delta), true)
+        .minus(fn(poolPairData, x, true))
+        .div(delta);
+    if (inverse) incrementalQuotient = bnum(1).div(incrementalQuotient);
+    const der_ans = der(poolPairData, x);
+    assert.approximately(
+        incrementalQuotient.div(der_ans).toNumber(),
+        1,
+        error,
+        'wrong result'
+    );
+}
