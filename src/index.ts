17c60794bcb72cee0f550cf8f00ba6852382b4fd
johngrantuk <johngrantuk@googlemail.com>
2021-11-09 15:16:54 +0000

    Added frontend queryBatchSwapHelpers.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export { SOR } from './wrapper';
 export { BPTForTokensZeroPriceImpact as weightedBPTForTokensZeroPriceImpact } from './frontendHelpers/weightedHelpers';
 export { BPTForTokensZeroPriceImpact as stableBPTForTokensZeroPriceImpact } from './frontendHelpers/stableHelpers';
+export {
+    queryBatchSwapTokensIn,
+    queryBatchSwapTokensInUpdateAmounts,
+    queryBatchSwapTokensOut,
+    queryBatchSwapTokensOutUpdateAmounts,
+} from './frontendHelpers/queryBatchSwapHelpers';
 export * from './types';
