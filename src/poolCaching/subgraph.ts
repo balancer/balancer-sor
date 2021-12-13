d33006232d6051cca5a0bd381c1787593faa9d56
johngrantuk <johngrantuk@googlemail.com>
2021-12-13 20:10:12 +0000

    Add network specific Subgraph queries.

--- src/poolCaching/subgraph.ts
+++ src/poolCaching/subgraph.ts
@@ -1,12 +1,7 @@
 import fetch from 'isomorphic-fetch';
 import { SubgraphPoolBase } from '../types';
 
-// Returns all public pools
-export async function fetchSubgraphPools(
-    subgraphUrl: string
-): Promise<SubgraphPoolBase[]> {
-    // can filter for publicSwap too??
-    const query = `
+const queryWithLinear = `
       {
         pools: pools(
           first: 1000,
@@ -42,15 +37,60 @@ export async function fetchSubgraphPools(
       }
     `;
 
+const queryWithOutLinear = `
+      {
+        pools: pools(
+          first: 1000,
+          where: { swapEnabled: true },
+          orderBy: totalLiquidity,
+          orderDirection: desc
+        ) {
+          id
+          address
+          poolType
+          swapFee
+          totalShares
+          tokens {
+            address
+            balance
+            decimals
+            weight
+            priceRate
+          }
+          tokensList
+          totalWeight
+          amp
+          expiryTime
+          unitSeconds
+          principalToken
+          baseToken
+          swapEnabled
+        }
+      }
+    `;
+
+export const Query: { [chainId: number]: string } = {
+    1: queryWithLinear,
+    3: queryWithLinear,
+    4: queryWithLinear,
+    5: queryWithLinear,
+    42: queryWithLinear,
+    137: queryWithOutLinear,
+    42161: queryWithLinear,
+};
+
+// Returns all public pools
+export async function fetchSubgraphPools(
+    subgraphUrl: string,
+    chainId = 1
+): Promise<SubgraphPoolBase[]> {
     const response = await fetch(subgraphUrl, {
         method: 'POST',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            query,
-        }),
+        body: JSON.stringify({ query: Query[chainId] }),
     });
 
     const { data } = await response.json();
