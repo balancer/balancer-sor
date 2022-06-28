bbece4e96db50df36cfc359d15d12fda86b80d54
johngrantuk <johngrantuk@googlemail.com>
2022-06-28 14:25:08 +0100

    Add local fork testnet.

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require('@nomiclabs/hardhat-ethers');
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    networks: {
+        hardhat: {
+            chainId: 1,
+        },
+    },
+};
