6fa06536841514bc1804c480d09cf47b48e92984
johngrantuk <johngrantuk@googlemail.com>
2023-01-20 11:01:11 +0000

    Update onchain fetch. Add test for phantomStable.

---
+++ test/PhantomStable.integration.spec.ts
@@ -0,0 +1,271 @@
+// TS_NODE_PROJECT='tsconfig.testing.json' npx mocha -r ts-node/register test/PhantomStable.integration.spec.ts
+import dotenv from 'dotenv';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Vault__factory } from '@balancer-labs/typechain';
+import { vaultAddr } from './testScripts/constants';
+import { SubgraphPoolBase, SwapTypes, SOR } from '../src';
+import {
+    Network,
+    MULTIADDR,
+    SOR_CONFIG,
+    ADDRESSES,
+} from './testScripts/constants';
+import { OnChainPoolDataService } from './lib/onchainData';
+import { TokenPriceService } from '../src';
+import { AddressZero } from '@ethersproject/constants';
+import { parseFixed } from '@ethersproject/bignumber';
+import { expect } from 'chai';
+import { closeTo } from './lib/testHelpers';
+
+dotenv.config();
+
+let sor: SOR;
+const networkId = Network.MAINNET;
+const { ALCHEMY_URL: jsonRpcUrl } = process.env;
+const rpcUrl = 'http://127.0.0.1:8545';
+const provider = new JsonRpcProvider(rpcUrl, networkId);
+const vault = Vault__factory.connect(vaultAddr, provider);
+const bbausdt = ADDRESSES[networkId].bbausdc.address;
+const bbadai = ADDRESSES[networkId].bbadai.address;
+const bpt = ADDRESSES[networkId].bbausd.address;
+const funds = {
+    sender: AddressZero,
+    recipient: AddressZero,
+    fromInternalBalance: false,
+    toInternalBalance: false,
+};
+
+// bbausd (first interation with PhantomStable)
+const testPool: SubgraphPoolBase = {
+    id: '0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb20000000000000000000000fe',
+    address: '0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb2',
+    poolType: 'StablePhantom',
+    swapFee: '0.00001',
+    swapEnabled: true,
+    totalShares: '3102931.002983334165119968',
+    tokens: [
+        {
+            address: '0x2bbf681cc4eb09218bee85ea2a5d3d13fa40fc0c',
+            balance: '961289.310891979395980804',
+            decimals: 18,
+            weight: null,
+            priceRate: '1.017031850267034026',
+        },
+        {
+            address: '0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb2',
+            balance: '5192296855431896.625547162164100127',
+            decimals: 18,
+            weight: null,
+            priceRate: '1',
+        },
+        {
+            address: '0x804cdb9116a10bb78768d3252355a1b18067bf8f',
+            balance: '1083156.226776690537941682',
+            decimals: 18,
+            weight: null,
+            priceRate: '1.011887483271879349',
+        },
+        {
+            address: '0x9210f1204b5a24742eba12f710636d76240df3d0',
+            balance: '1075631.693123588669606737',
+            decimals: 18,
+            weight: null,
+            priceRate: '1.010970263031733395',
+        },
+    ],
+    tokensList: [
+        '0x2bbf681cc4eb09218bee85ea2a5d3d13fa40fc0c',
+        '0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb2',
+        '0x804cdb9116a10bb78768d3252355a1b18067bf8f',
+        '0x9210f1204b5a24742eba12f710636d76240df3d0',
+    ],
+    amp: '1472',
+};
+
+// Setup SOR with data services
+function setUp(networkId: Network, provider: JsonRpcProvider): SOR {
+    // The SOR needs to fetch pool data from an external source. This provider fetches from Subgraph and onchain calls.
+    const subgraphPoolDataService = new OnChainPoolDataService({
+        vaultAddress: vaultAddr,
+        multiAddress: MULTIADDR[networkId],
+        provider,
+        pools: [testPool],
+    });
+
+    class CoingeckoTokenPriceService implements TokenPriceService {
+        constructor(private readonly chainId: number) {}
+        async getNativeAssetPriceInToken(
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            tokenAddress: string
+        ): Promise<string> {
+            return '0';
+        }
+    }
+
+    // Use coingecko to fetch token price information. Used to calculate cost of additonal swaps/hops.
+    const coingeckoTokenPriceService = new CoingeckoTokenPriceService(
+        networkId
+    );
+
+    return new SOR(
+        provider,
+        SOR_CONFIG[networkId],
+        subgraphPoolDataService,
+        coingeckoTokenPriceService
+    );
+}
+describe('PhantomStable', () => {
+    context('test swaps vs queryBatchSwap', () => {
+        // Setup chain
+        before(async function () {
+            this.timeout(20000);
+
+            await provider.send('hardhat_reset', [
+                {
+                    forking: {
+                        jsonRpcUrl,
+                        blockNumber: 16447247,
+                    },
+                },
+            ]);
+
+            sor = setUp(networkId, provider);
+            await sor.fetchPools();
+        });
+
+        context('ExactIn', () => {
+            it('token>token', async () => {
+                const swapType = SwapTypes.SwapExactIn;
+
+                const swapInfo = await sor.getSwaps(
+                    bbausdt,
+                    bbadai,
+                    swapType,
+                    parseFixed('2301.456', 18)
+                );
+
+                const queryResult = await vault.callStatic.queryBatchSwap(
+                    swapType,
+                    swapInfo.swaps,
+                    swapInfo.tokenAddresses,
+                    funds
+                );
+                expect(queryResult[0].toString()).to.eq(
+                    swapInfo.swapAmount.toString()
+                );
+                closeTo(queryResult[1].abs(), swapInfo.returnAmount, 1);
+            }).timeout(10000);
+            it('token>bpt', async () => {
+                const swapType = SwapTypes.SwapExactIn;
+
+                const swapInfo = await sor.getSwaps(
+                    bbausdt,
+                    bpt,
+                    swapType,
+                    parseFixed('2301.456', 18)
+                );
+
+                const queryResult = await vault.callStatic.queryBatchSwap(
+                    swapType,
+                    swapInfo.swaps,
+                    swapInfo.tokenAddresses,
+                    funds
+                );
+                expect(queryResult[0].toString()).to.eq(
+                    swapInfo.swapAmount.toString()
+                );
+                expect(queryResult[1].abs().toString()).to.eq(
+                    swapInfo.returnAmount.toString()
+                );
+            }).timeout(10000);
+            it('bpt>token', async () => {
+                const swapType = SwapTypes.SwapExactIn;
+
+                const swapInfo = await sor.getSwaps(
+                    bpt,
+                    bbausdt,
+                    swapType,
+                    parseFixed('2301.456', 18)
+                );
+
+                const queryResult = await vault.callStatic.queryBatchSwap(
+                    swapType,
+                    swapInfo.swaps,
+                    swapInfo.tokenAddresses,
+                    funds
+                );
+                expect(queryResult[0].toString()).to.eq(
+                    swapInfo.swapAmount.toString()
+                );
+                closeTo(queryResult[1].abs(), swapInfo.returnAmount, 1);
+            }).timeout(10000);
+        });
+
+        context('ExactOut', () => {
+            const swapType = SwapTypes.SwapExactOut;
+
+            it('token>token', async () => {
+                const swapInfo = await sor.getSwaps(
+                    bbadai,
+                    bbausdt,
+                    swapType,
+                    parseFixed('0.1', 18)
+                );
+
+                const queryResult = await vault.callStatic.queryBatchSwap(
+                    swapType,
+                    swapInfo.swaps,
+                    swapInfo.tokenAddresses,
+                    funds
+                );
+                // Amount out should be exact
+                expect(queryResult[1].abs().toString()).to.eq(
+                    swapInfo.swapAmount.toString()
+                );
+                closeTo(queryResult[0].abs(), swapInfo.returnAmount, 2);
+            }).timeout(10000);
+            it('token>bpt', async () => {
+                const swapInfo = await sor.getSwaps(
+                    bbadai,
+                    bpt,
+                    swapType,
+                    parseFixed('1234.5678', 18)
+                );
+
+                const queryResult = await vault.callStatic.queryBatchSwap(
+                    swapType,
+                    swapInfo.swaps,
+                    swapInfo.tokenAddresses,
+                    funds
+                );
+                // Amount out should be exact
+                expect(queryResult[1].abs().toString()).to.eq(
+                    swapInfo.swapAmount.toString()
+                );
+                closeTo(queryResult[0].abs(), swapInfo.returnAmount, 2);
+            }).timeout(10000);
+            it('bpt>token', async () => {
+                const swapInfo = await sor.getSwaps(
+                    bpt,
+                    bbadai,
+                    swapType,
+                    parseFixed('987.2345', 18)
+                );
+
+                const queryResult = await vault.callStatic.queryBatchSwap(
+                    swapType,
+                    swapInfo.swaps,
+                    swapInfo.tokenAddresses,
+                    funds
+                );
+                // Amount out should be exact
+                expect(queryResult[1].abs().toString()).to.eq(
+                    swapInfo.swapAmount.toString()
+                );
+                expect(queryResult[0].toString()).to.eq(
+                    swapInfo.returnAmount.toString()
+                );
+            }).timeout(10000);
+        });
+    });
+});
--- test/lib/onchainData.ts
+++ test/lib/onchainData.ts
@@ -8,6 +8,7 @@ import vaultAbi from '../../src/abi/Vault.json';
 import aTokenRateProvider from '../../src/abi/StaticATokenRateProvider.json';
 import weightedPoolAbi from '../../src/pools/weightedPool/weightedPoolAbi.json';
 import stablePoolAbi from '../../src/pools/stablePool/stablePoolAbi.json';
+import composableDtablePoolAbi from '../../src/pools/composableStable/ComposableStable.json';
 import elementPoolAbi from '../../src/pools/elementPool/ConvergentCurvePool.json';
 import linearPoolAbi from '../../src/pools/linearPool/linearPoolAbi.json';
 import { PoolFilter, SubgraphPoolBase, PoolDataService } from '../../src';
@@ -33,6 +34,7 @@ export async function getOnChainBalances(
                     ...stablePoolAbi,
                     ...elementPoolAbi,
                     ...linearPoolAbi,
+                    ...composableDtablePoolAbi,
                 ].map((row) => [row.name, row])
             )
         );
@@ -53,6 +55,35 @@ export async function getOnChainBalances(
             pool.id,
         ]);
         multiPool.call(`${pool.id}.totalSupply`, pool.address, 'totalSupply');
+
+        // Pools with pre minted BPT
+        if (
+            pool.poolType.includes('Linear') ||
+            pool.poolType === 'StablePhantom'
+        ) {
+            multiPool.call(
+                `${pool.id}.virtualSupply`,
+                pool.address,
+                'getVirtualSupply'
+            );
+        }
+
+        /**
+         * Returns the effective BPT supply.
+         * In other pools, this would be the same as `totalSupply`, but there are two key differences here:
+         *  - this pool pre-mints BPT and holds it in the Vault as a token, and as such we need to subtract the Vault's
+         *    balance to get the total "circulating supply". This is called the 'virtualSupply'.
+         *  - the Pool owes debt to the Protocol in the form of unminted BPT, which will be minted immediately before the
+         *    next join or exit. We need to take these into account since, even if they don't yet exist, they will
+         *    effectively be included in any Pool operation that involves BPT.
+         * In the vast majority of cases, this function should be used instead of `totalSupply()`.
+         */
+        if (pool.poolType === 'ComposableStable')
+            multiPool.call(
+                `${pool.id}.actualSupply`,
+                pool.address,
+                'getActualSupply'
+            );
 
         // TO DO - Make this part of class to make more flexible?
         if (
@@ -123,6 +154,9 @@ export async function getOnChainBalances(
                 balances: string[];
             };
             rate?: string;
+            totalSupply: string;
+            virtualSupply?: string;
+            actualSupply?: string;
         }
     >;
 
@@ -138,9 +172,13 @@ export async function getOnChainBalances(
                     balances: string[];
                 };
                 rate?: string;
+                totalSupply: string;
+                virtualSupply?: string;
+                actualSupply?: string;
             }
         >;
     } catch (err) {
+        console.log(err);
         throw `Issue with multicall execution.`;
     }
 
@@ -148,7 +186,14 @@ export async function getOnChainBalances(
 
     Object.entries(pools).forEach(([poolId, onchainData], index) => {
         try {
-            const { poolTokens, swapFee, weights } = onchainData;
+            const {
+                poolTokens,
+                swapFee,
+                weights,
+                virtualSupply,
+                actualSupply,
+                totalSupply,
+            } = onchainData;
 
             if (
                 subgraphPools[index].poolType === 'Stable' ||
@@ -212,6 +257,36 @@ export async function getOnChainBalances(
                     T.weight = formatFixed(weights[i], 18);
                 }
             });
+            // Pools with pre minted BPT
+            if (
+                subgraphPools[index].poolType.includes('Linear') ||
+                subgraphPools[index].poolType === 'StablePhantom'
+            ) {
+                if (virtualSupply === undefined) {
+                    console.error(
+                        `Pool with pre-minted BPT missing Virtual Supply: ${poolId}`
+                    );
+                    return;
+                }
+                subgraphPools[index].totalShares = formatFixed(
+                    virtualSupply,
+                    18
+                );
+            } else if (subgraphPools[index].poolType === 'ComposableStable') {
+                if (actualSupply === undefined) {
+                    console.error(
+                        `ComposableStable missing Actual Supply: ${poolId}`
+                    );
+                    return;
+                }
+                console.log(`!!!!!!!!!`, actualSupply.toString());
+                subgraphPools[index].totalShares = formatFixed(
+                    actualSupply,
+                    18
+                );
+            } else {
+                subgraphPools[index].totalShares = formatFixed(totalSupply, 18);
+            }
             onChainPools.push(subgraphPools[index]);
         } catch (err) {
             throw `Issue with pool onchain data: ${err}`;
