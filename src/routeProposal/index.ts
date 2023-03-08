7791832768b2caf54214a4c0ecad9c0f19193d8e
aplki <aplki@protonmail.com>
2023-03-08 14:35:35 +0800

    removed added space

--- src/routeProposal/index.ts
+++ src/routeProposal/index.ts
@@ -32,7 +32,6 @@ export class RouteProposer {
     ): NewPath[] {
         tokenIn = tokenIn.toLowerCase();
         tokenOut = tokenOut.toLowerCase();
-
         if (pools.length === 0) return [];
 
         // If token pair has been processed before that info can be reused to speed up execution
