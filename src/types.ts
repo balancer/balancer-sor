24848e48e6021736a11bf282f3f879cf4d5fda34
bronco <rg3mvdb0@protonmail.com>
2023-03-31 18:09:57 +0200

    feature: pass query arguments to data service
    When using SOR to only find a specific route between two well served tokens, eg: ETH, BTC there is no need to fetch all the Balancer pools and just few pools with the most liquidity for these tokens should cover 90% of the cases.
    
    This PR adds an optional parameter to fetchPools function allowing for fetching only specified pools we want to use in route building.

--- src/types.ts
+++ src/types.ts
@@ -270,7 +270,7 @@ export interface TokenPriceService {
 }
 
 export interface PoolDataService {
-    getPools(): Promise<SubgraphPoolBase[]>;
+    getPools(query?: GraphQLArgs): Promise<SubgraphPoolBase[]>;
 }
 
 export type FundManagement = {
@@ -279,3 +279,23 @@ export type FundManagement = {
     fromInternalBalance: boolean;
     toInternalBalance: boolean;
 };
+
+type GraphQLFilterOperator = 'gt' | 'lt' | 'eq' | 'in' | 'not_in' | 'contains';
+
+type GraphQLFilter = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [operator in GraphQLFilterOperator]?: any;
+};
+
+export interface GraphQLArgs {
+    chainId?: number;
+    first?: number;
+    skip?: number;
+    nextToken?: string;
+    orderBy?: string;
+    orderDirection?: string;
+    block?: {
+        number?: number;
+    };
+    where?: Record<string, GraphQLFilter>;
+}
