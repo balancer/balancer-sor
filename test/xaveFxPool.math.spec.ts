adc7836622f2a7fc44cb220943cf40967f7d903c
andreiashu <andreiashu@gmail.com>
2023-06-14 20:09:54 +0700

    🍁 added xaveFxPool.math.spec.ts

---
+++ test/xaveFxPool.math.spec.ts
@@ -0,0 +1,36 @@
+// TS_NODE_PROJECT='tsconfig.testing.json' npx mocha -r ts-node/register test/xaveFxPool.math.spec.ts
+require('dotenv').config();
+import { expect } from 'chai';
+import { bnum } from '../src/utils/bignumber';
+
+import {
+    viewRawAmount,
+    viewNumeraireAmount,
+} from '../src/pools/xaveFxPool/fxPoolMath';
+
+context('test fxMath functions', () => {
+    const tokenDecimals = bnum(6);
+    const tokenFxRateDecimals = bnum(8);
+    const rate = bnum('74376600'); // 0.743766
+    it(`should correctly return 'viewRawAmount'`, async () => {
+        const rawAmount = viewRawAmount(
+            bnum('10000'),
+            tokenDecimals,
+            rate,
+            tokenFxRateDecimals
+        );
+        const expected = '13445088912';
+        expect(rawAmount.toString()).to.eq(expected);
+    });
+
+    it(`should correctly return 'viewNumeraireAmount' values`, async () => {
+        const numerarieAmount = viewNumeraireAmount(
+            bnum('13445088912'),
+            tokenDecimals,
+            rate,
+            tokenFxRateDecimals
+        );
+        const expected = '9999.999999';
+        expect(numerarieAmount.toString()).to.eq(expected);
+    });
+});
